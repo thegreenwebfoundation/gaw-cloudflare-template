37ebf30cb849ef6006bf8ee06cb62949378f3282
fershad <27988517+fershad@users.noreply.github.com>
2025-05-12 21:03:12 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import gridAwareAuto from '@greenweb/gaw-plugin-cloudflare-workers';
+
+export default {
+	async fetch(request, env, ctx) {
+		return gridAwareAuto(request, env, ctx, {
+			gawDataApiKey: env.EMAPS_API_KEY,
+			kvCacheData: true,
+		});
+	},
+};
